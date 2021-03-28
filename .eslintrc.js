f0bf7b1e07740dfce88707dff87b57ab4afc528e
清水紀行 <noriyuki.shimizu0612@gmail.com>
2021-03-28 14:47:52 +0900

    eslint の設定まわりの見直し

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,40 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
+    root: true,
+    env: {
+        node: true
     },
-  ],
+    extends: [
+        'eslint:recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:vue/essential',
+        // 'plugin:@intlify/vue-i18n/recommended',
+        '@vue/airbnb',
+        '@vue/typescript',
+        'prettier',
+        'prettier/vue'
+    ],
+    plugins: ['import'],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    },
+    // settings: {
+    //     'vue-i18n': {
+    //         localeDir: './src/assets/i18n/*.{json,json5,yaml,yml}',
+    //         messageSyntaxVersion: '8.18.2'
+    //     }
+    // },
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module'
+    },
+    overrides: [
+        {
+            files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+            env: {
+                jest: true
+            }
+        }
+    ]
 };
