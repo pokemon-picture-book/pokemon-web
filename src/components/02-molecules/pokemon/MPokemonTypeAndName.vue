0c5e67e2c146362aaf16aa30a17b0b9d8215da72
清水紀行 <noriyuki.shimizu0612@gmail.com>
2021-05-01 15:36:06 +0900

    ポケモンのタイプと名称をセットにした molecule を追加

---
+++ src/components/02-molecules/pokemon/MPokemonTypeAndName.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="m-pokemon-type-and-name">
+        <a-type :items="typeItems" />
+        <span class="m-pokemon-type-and-name__name">{{ name }}</span>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import AType from '@/components/01-atoms/pokemon/AType.vue';
+import { ATypeItem } from 'types/pokemon/type';
+
+/**
+ * ポケモン名称.
+ */
+export default defineComponent({
+    components: {
+        AType
+    },
+    props: {
+        typeItems: {
+            type: Array as PropType<ATypeItem[]>,
+            default: []
+        },
+        name: {
+            type: String,
+            default: 'undefined'
+        }
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+.m-pokemon-type-and-name {
+    display: flex;
+    align-items: center;
+
+    &__name {
+        margin: 8px;
+    }
+}
+</style>
---
+++ src/stories/02-molecules/pokemon/MPokemonTypeAndName.stories.ts
@@ -0,0 +1,32 @@
+import MPokemonTypeAndName from '@/components/02-molecules/pokemon/MPokemonTypeAndName.vue';
+import { ATypeItem } from 'types/pokemon/type';
+
+export default {
+    title: 'Molecules/m-pokemon-type-and-name'
+};
+
+export const TypeAndName = () => ({
+    components: { MPokemonTypeAndName },
+    data: () => {
+        return {
+            typeItems: [
+                { code: 'poison' },
+                { code: 'grass' }
+            ] as ATypeItem[]
+        }
+    },
+    template: '<m-pokemon-type-and-name name="フシギダネ" :type-items="typeItems" />',
+});
+
+export const TypeNameAndName = () => ({
+    components: { MPokemonTypeAndName },
+    data: () => {
+        return {
+            typeItems: [
+                { code: 'poison', label: 'どく' },
+                { code: 'grass', label: 'くさ' }
+            ] as ATypeItem[]
+        }
+    },
+    template: '<m-pokemon-type-and-name name="フシギダネ" :type-items="typeItems" />',
+});
