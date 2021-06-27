5743732bb16961f1eeae396fe05959a6c1935697
清水紀行 <noriyuki.shimizu0612@gmail.com>
2021-06-27 19:01:01 +0900

    stylelint の導入

---
+++ stylelint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    extends: 'stylelint-config-standard',
+    plugins: [
+        'stylelint-scss'
+    ],
+    rules: {
+        indentation: 4,
+        'at-rule-no-unknown': null,
+        'scss/at-rule-no-unknown': true,
+        'selector-pseudo-element-no-unknown': [
+            true,
+            {
+                ignorePseudoElements: ['/^v-/', 'pseudo-element'],
+            }
+        ]
+    }
+};
