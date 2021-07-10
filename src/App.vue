5654d1dc1e550bb6732f83643ada3d9a50f4dfc1
清水紀行 <noriyuki.shimizu0612@gmail.com>
2021-07-10 17:33:05 +0900

    不要なソース・ファイルを削除

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-    <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
+    <div>
+        <router-view />
     </div>
-    <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
 
 <style lang="scss">
 body {
     font-size: 16px;
-    font-smooth: never;
     -webkit-font-smoothing: none;
     font-family: 'pokemon-font', monospace;
 }
