f1ab6d5d6b8bf9cd7fab2127b6169abf3978aa69
清水紀行 <noriyuki.shimizu0612@gmail.com>
2021-07-17 19:41:53 +0900

    Suspense コンポーネントに対応するよう storybook のソースを修正

--- src/stories/03-organisms/pokemon/OPokemonList.stories.ts
+++ src/stories/03-organisms/pokemon/OPokemonList.stories.ts
@@ -1,12 +1,20 @@
 import OPokemonList from '@/components/03-organisms/pokemon/OPokemonList.vue';
+import OSpinner from '@/components/03-organisms/global/OSpinner.vue';
 
 export default {
     title: 'Organisms/Pokemon/o-pokemon-list'
 };
 
 export const Simple = () => ({
-    components: { OPokemonList },
+    components: { OPokemonList, OSpinner },
     template: `
-    <o-pokemon-list></o-pokemon-list>
+    <Suspense>
+        <template #default>
+            <o-pokemon-list />
+        </template>
+        <template #fallback>
+            <o-spinner />
+        </template>
+    </Suspense>
     `
 });
