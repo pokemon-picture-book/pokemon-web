cae0f0e5d3001d78490386c3fb086d2f4e1d0c59
清水紀行 <noriyuki.shimizu0612@gmail.com>
2021-08-14 13:05:55 +0900

    構成を変更

--- src/components/09-layout/WebLayout.vue
+++ src/components/09-layout/WebLayout.vue
@@ -1,19 +1,26 @@
 <template>
-    <div>
-        <o-header />
-    </div>
-    <div>
-        <router-view />
-    </div>
+    <Suspense>
+        <template #default>
+            <div>
+                <o-header />
+                <router-view />
+            </div>
+        </template>
+        <template #fallback>
+            <o-spinner />
+        </template>
+    </Suspense>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import OHeader from '@/components/03-organisms/global/OHeader.vue';
+import OSpinner from '@/components/03-organisms/global/OSpinner.vue';
 
 export default defineComponent({
     components: {
-        OHeader
+        OHeader,
+        OSpinner
     }
 });
 </script>
