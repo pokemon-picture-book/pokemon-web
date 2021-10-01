e7da985c3ba2dab197c5eb4b3cbaa613fe5c2881
清水紀行 <noriyuki.shimizu0612@gmail.com>
2021-10-01 09:25:20 +0900

    vue-cli から vite へビルドツールリプレース

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-    preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+    moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
     transform: {
+        '^.+\\.ts$': 'ts-jest',
         '^.+\\.vue$': 'vue-jest'
+    },
+    moduleNameMapper: {
+        '@/(.*)': '<rootDir>/src/$1'
     }
 };
